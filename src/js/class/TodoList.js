71b666785a9b538f808318ae89b33978dc9372a2
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-02-17 21:03:44 +0100

    add/delete todo item

--- src/js/class/TodoList.js
+++ src/js/class/TodoList.js
@@ -7,20 +7,33 @@ export class TodoList {
     constructor(todosArray, listEl) {
         this.todoItems = todosArray || [];
         this.listEl = listEl;
+
+        this.listEl.addEventListener("click", event => {
+            console.log("todo click event", event);
+
+            if(event.target.classList.contains("delete")) {
+                event.target.parentElement.remove();
+                let indexToRemove = event.target.parentElement.dataset.index;
+
+            } // delete button kattintas$
+        });
     }
 
     addTodo(todo) {
         if(todo instanceof Todo) {
+            this.#createElement(todo, this.todoItems.length);
             this.todoItems.push(todo);
-            this.#createElement(todo);
         } else throw Error("You can only add a Todo instance!");
+
+        console.log("log todoitems", this.todoItems );
     }
 
-    #createElement(todo) {
-        const liElement = document.createElement("li");
-        liElement.innerText = todo.description + ", " + todo.duedate + ", " + todo.priority;
-        this.listEl.append(liElement);
+    #createElement(todo, index) {
+        const newLiEl = document.createElement('li');
+        const listElementTemplate = `${todo.printDetails()} <button class="delete">delete</button>`
+        newLiEl.dataset.index = index;
+        newLiEl.innerHTML = listElementTemplate;
+
+        this.listEl.append(newLiEl);
     }
-
-
 }
